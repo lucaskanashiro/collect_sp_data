d0cb2384754d5deed7d5adb953d89de05e1d2869
Lucas Kanashiro <kanashiro@debian.org>
2017-02-06 14:37:57 -0200

    increase number of elements shown in each page

--- app/app/controllers/air_qualities_controller.rb
+++ app/app/controllers/air_qualities_controller.rb
@@ -4,6 +4,6 @@ class AirQualitiesController < ApplicationController
   # GET /air_qualities
   # GET /air_qualities.json
   def index
-    @air_qualities = AirQuality.paginate(:page => params[:page], :per_page => 30)
+    @air_qualities = AirQuality.paginate(:page => params[:page], :per_page => 50)
   end
 end
--- app/app/controllers/weathers_controller.rb
+++ app/app/controllers/weathers_controller.rb
@@ -4,6 +4,6 @@ class WeathersController < ApplicationController
   # GET /weathers
   # GET /weathers.json
   def index
-    @weathers = Weather.paginate(:page => params[:page], :per_page => 30)
+    @weathers = Weather.paginate(:page => params[:page], :per_page => 50)
   end
 end
