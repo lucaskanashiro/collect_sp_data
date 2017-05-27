7c41d05207a58fffa85bab690c69184eda08efc8
Dylan Guedes <djmgguedes@gmail.com>
2017-05-27 12:09:50 -0300

    update air_quality entity to send data to interscity instance
    Signed-off-by: Dylan Guedes <djmgguedes@gmail.com>

--- air_quality.rb
+++ air_quality.rb
@@ -3,6 +3,8 @@ require 'mechanize'
 require 'mongo'
 require 'yaml'
 require 'time'
+require 'rest-client'
+require 'json'
 
 project_path = "./"
 project_path = ARGV[0] unless ARGV[0].nil?
@@ -16,7 +18,65 @@ url_cetesb = "http://sistemasinter.cetesb.sp.gov.br/Ar/php/ar_resumo_hora.php"
 
 agent = Mechanize.new
 page = agent.get(url_cetesb)
+page.encoding = "utf-8"
 table = page.search("table tr td table.font01")[0]
+
+class AirQuality
+  attr_accessor :status, :description, :registered, :region, :quality,
+    :uuid, :timestamp, :index, :polluting
+  def initialize(params={})
+    params.each do |key, value|
+      instance_variable_set("@#{key}", value)
+    end
+    self.status = "active"
+    self.description = "#{self.region} air quality"
+    self.registered = false
+  end
+
+  def capabilities
+    ["air-quality", "polluting-index", "polluting"]
+  end
+
+  def register_resource
+    url = ENV["INTERSCITY_ADAPTOR_HOST"] + "/components"
+
+    doc = {
+      lat: -23.559616,
+      lon: -1.55,
+      description: self.description,
+      capabilities: self.capabilities,
+      status: "active",
+    }
+
+    begin
+      response = RestClient.post(url, {data: doc})
+      response = JSON.parse(response)
+      self.uuid = response["data"]["uuid"]
+      self.registered = true
+    rescue RestClient::Exception => e
+      puts "ERROR: Could not register resource. Description: #{e}"
+    end
+  end
+
+  def send_data
+    url = ENV["INTERSCITY_ADAPTOR_HOST"] + "/components/#{self.uuid}/data"
+
+    doc = {
+      air_quality: [{value: self.quality, timestamp: self.timestamp}],
+      polluting_index: [{value: self.index, timestamp: self.timestamp}],
+      polluting: [{value: self.polluting, timestamp: self.timestamp}]
+    }
+
+    begin
+      response = RestClient.post(url, {data: doc})
+      self.registered = true
+    rescue RestClient::Exception => e
+      puts "ERROR: Could not send data from resource. Description: #{e.response}"
+    end
+  end
+end
+
+resources = {}
 
 table.element_children.each do |line|
 	next if line.element_children.empty?
@@ -37,11 +97,29 @@ table.element_children.each do |line|
 
  timestamp = Time.now.getutc.to_s
 
-	doc = { region: region,
-         quality: quality,
-         index: index,
-         polluting: polluting,
-         timestamp: timestamp }
+	doc = {
+    region: region,
+    quality: quality,
+    index: index,
+    polluting: polluting,
+    timestamp: timestamp
+  }
 
 	collection.insert_one(doc)
+
+  if ENV["USE_INTERSCITY"] && ENV["INTERSCITY_ADAPTOR_HOST"]
+    aq = AirQuality.new(doc)
+    aq.register_resource
+    if aq.registered
+      resources.update("#{aq.region}" => aq)
+    end
+  else
+    puts ">>> InterSCity configuration not found <<<"
+  end
+end
+
+if ENV["USE_INTERSCITY"] && ENV["INTERSCITY_ADAPTOR_HOST"]
+  resources.each do |key, entity|
+    entity.send_data
+  end
 end
