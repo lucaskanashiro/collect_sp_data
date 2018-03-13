e9f2297b19e8701bd809b5854ebc1f190c359b0c
DylanGuedes <djmgguedes@gmail.com>
2018-03-13 19:27:36 -0300

    adds olho_vivo collector
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

---
+++ olho_vivo.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'json'
+require 'yaml'
+require 'mongo'
+require 'rest-client'
+require 'colorize'
+
+project_path = "./"
+project_path = ARGV[0] unless ARGV[0].nil?
+load "#{project_path}/interscity_entity.rb"
+
+config = YAML.load_file("#{project_path}settings.yml")
+
+db = Mongo::Client.new([ config["DATABASE_HOST"] ], :database => config["DATABASE_NAME"])
+collection = db[:olho_vivo]
+
+token = ENV["OLHO_VIVO_TOKEN"]
+
+if not token
+  raise "MISSING OLHO_VIVO TOKEN. Set the environment variable `olho_vivo_token`!"
+end
+
+url = "http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token=#{token}"
+response = RestClient.post(url, {})
+auth = response.cookies['apiCredentials']
+
+url = "http://api.olhovivo.sptrans.com.br/v2.1/Posicao"
+response = RestClient.get(url, {
+  cookies: {
+    apiCredentials: auth
+  }
+})
+body = JSON.parse(response)
+
+body["l"].each do |line|
+  line["vs"].each do |vehicle|
+    doc = {
+      prefix: vehicle["p"],
+      lat: vehicle["py"],
+      lon: vehicle["px"],
+      timestamp: vehicle["ta"],
+      display: line["c"],
+      identifier_code: line["cl"],
+      direction: line["sl"],
+      display_origin: line["lt0"],
+      display_destination: line["lt1"],
+      vehicles_count: line["qv"]
+    }
+
+    collection.insert_one(doc)
+  end
+end
